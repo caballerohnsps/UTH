70041c546a4796f8a2218f203e1985da4659dcd2
oscar caballero <caballerohnsps@gmail.com>
2024-01-25 19:10:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/programovil1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.programovil1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
